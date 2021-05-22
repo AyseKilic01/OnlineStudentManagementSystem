13e6a4dde69d3e852ac2feb49faf27efc0eddb38
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-22 18:55:29 +0300

    added n  layers

---
+++ Entities/Concrete/Admin.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Admin : IEntity
+    {
+        public int AID { get; set; }
+        public string Name { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+    }
+}
