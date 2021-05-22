a64533e8496db43af9ca22f87bfdfff0fbccbaa1
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-22 19:18:17 +0300

    complete base classes

---
+++ Business/Concrete/CourseManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CourseManager : IService<Course>
+    {
+        private ICourseDAL _dal;
+
+        public CourseManager(ICourseDAL productDal)
+        {
+            _dal = productDal;
+        }
+
+        public List<Course> GetAll()
+        {
+            return _dal.GetAll();
+
+        }
+    }
+}
